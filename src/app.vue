45e483945f75caff5d4563606e79a62960eb437a
chayne <zhoucy@anche.cn>
2024-09-27 16:28:17 +0800

    chore: 配置seo

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+// useHead({
+//   title: '我的应用',
+//   meta: [{ name: 'description', content: '我的神奇网站。' }],
+//   bodyAttrs: {
+//     class: 'test',
+//   },
+//   script: [{ innerHTML: "console.log('Hello world')" }],
+// });
+const title = ref('我的神奇网站');
+useSeoMeta({
+  title,
+  ogTitle: '我的神奇网站',
+  description: ['这是我的神奇网站，让我来告诉你关于它的一切。', '服务端渲染，useHead的使用，Nuxt3,Nuxt3服务端渲染'],
+  ogDescription: '这是我的神奇网站，让我来告诉你关于它的一切。',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+});
+</script>
+
 <template>
   <div>
     <NuxtLayout>
