e13a8f1b16579ebccb009b752533dcc30f06d7eb
chayne <zhoucy@anche.cn>
2024-09-29 16:41:33 +0800

    chore:  添加返回首页

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const router = useRoute();
+</script>
 
 <template>
   <div>
     一个默认模板页面
     <slot></slot>
+    <template v-if="router.path !== '/'">
+      <NuxtLink to="/"> back to Home </NuxtLink>
+    </template>
   </div>
 </template>
