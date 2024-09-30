5d53029f68ec8d17abee5fd061aee9f066b419d5
chayne <zhoucy@anche.cn>
2024-09-30 14:41:30 +0800

    chore: 样式优化

--- src/pages/three/gltfView.vue
+++ src/pages/three/gltfView.vue
@@ -5,24 +5,45 @@ import Dinosaure from './components/Dinosaure.vue';
 import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three';
 
 const gl = {
-  clearColor: '#F78B3D',
+  // clearColor: '#F78B3D',
   shadows: true,
-  alpha: false,
+  alpha: true,
   shadowMapType: BasicShadowMap,
   outputColorSpace: SRGBColorSpace,
   toneMapping: NoToneMapping,
-  windowSize: true,
+  // windowSize: true,
 };
 </script>
 <template>
-  <TresCanvas v-bind="gl">
-    <TresPerspectiveCamera :position="[2, 2, 3]" :look-at="[0, 0.5, 0]" />
-    <!-- 鼠标控制模型移动 -->
-    <!-- <OrbitControls /> -->
-    <Suspense>
-      <Dinosaure></Dinosaure>
-    </Suspense>
-    <TresDirectionalLight color="#F78B3D" :position="[3, 3, 3]" :intensity="1" />
-    <TresAmbientLight :intensity="2" />
-  </TresCanvas>
+  <div class="canvas-page">
+    <div class="canvas-model">
+      <TresCanvas v-bind="gl">
+        <TresPerspectiveCamera :position="[2, 2, 3]" :look-at="[0, 0.5, 0]" />
+        <!-- 鼠标控制模型移动 -->
+        <!-- <OrbitControls /> -->
+        <Suspense>
+          <Dinosaure></Dinosaure>
+        </Suspense>
+        <TresDirectionalLight color="#F78B3D" :position="[3, 3, 3]" :intensity="1" />
+        <TresAmbientLight :intensity="2" />
+      </TresCanvas>
+    </div>
+    <div class="canvas-article bg-gradient-to-b from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-100%"> </div>
+  </div>
 </template>
+<style lang="scss" scoped>
+.canvas-page {
+  width: 100%;
+  position: relative;
+  .canvas-model {
+    width: 100%;
+    height: 800px;
+    position: fixed;
+    top: 0;
+  }
+  .canvas-article {
+    width: 100%;
+    height: 3300px;
+  }
+}
+</style>
