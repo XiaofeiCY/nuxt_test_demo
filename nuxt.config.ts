31382a9e6c518c6573778bf7bbc73d6a327b681e
chayne <zhoucy@anche.cn>
2024-09-30 09:51:36 +0800

    chore: 调整图片导入补充

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from 'nuxt/config';
+// import { fileURLToPath } from 'url';
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   // devtools: { enabled: true },
   srcDir: 'src/',
-  image: {
-    dir: 'assets/images',
-    // formats: ['webp', 'avif'],
-  },
-  modules: [
-    '@pinia/nuxt',
-    '@vueuse/nuxt',
-    '@tresjs/nuxt',
-    '@nuxtjs/i18n',
-    '@nuxt/image',
-    '@nuxtjs/tailwindcss',
-  ],
+  // image: {
+  //   dir: 'assets/images',
+  //   // formats: ['webp', 'avif'],
+  // },
+  // alias: {
+  //   images: fileURLToPath(new URL('src/assets/images', import.meta.url)),
+  // },
+  modules: ['@pinia/nuxt', '@vueuse/nuxt', '@tresjs/nuxt', '@nuxtjs/i18n', '@nuxt/image', '@nuxtjs/tailwindcss'],
   i18n: {
     vueI18n: './i18n.config.ts',
   },
--- src/pages/picDemo.vue
+++ src/pages/picDemo.vue
@@ -5,14 +5,15 @@
     <NuxtLink to="https://ithelp.ithome.com.tw/articles/10307570"> 图片存放位置区别 </NuxtLink>
     <NuxtLink to="https://ithelp.ithome.com.tw/articles/10330879"> 图片引用路径方式 </NuxtLink>
 
-    <!-- <div>
+    <div>
       <h3> 存放在public下面的图片：public/images/publicDemo.jpg </h3>
-      <NuxtImg src="/images/publicDemo.jpg" />
-    </div> -->
+      <NuxtImg src="/images/publicDemo.jpg" width="300" />
+    </div>
     <div>
       <h3> 存放在assets下面的图片：assets/images/assetsDemo.jpg </h3>
-      <p>放在assets下，需要在nuxtConfig文件配置图片路径，而public则不需要</p>
-      <NuxtImg src="/assetsDemo.jpg" width="300" />
+      <p>放在assets下的图片，如果想用NuxtImg标签导入图片，需要在nuxtConfig文件配置dir图片路径，而public则不需要</p>
+      <p>否则直接使用img标签即可</p>
+      <img src="~/assets/images/assetsDemo.jpg" width="300" />
     </div>
   </div>
 </template>
