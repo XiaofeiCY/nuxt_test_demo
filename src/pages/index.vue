085b87908b5c30c10b60a44e8abb6d2257da1b89
chayne <zhoucy@anche.cn>
2024-09-30 10:23:26 +0800

    chore: 样式优化

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -28,27 +28,27 @@ const toggleDark = useToggle(isDark);
 </script>
 
 <template>
-  <div class="section shadow-sm"> 这是第一个页面{{ xx }} </div>
-  <div class="section shadow-sm bg-gray-300">
+  <div class="section shadow-xl bg-zinc-300"> 这是第一个页面{{ xx }} </div>
+  <div class="section shadow-xl bg-zinc-300">
     <NuxtLink to="/about">去about页面：{{ userInfo.userName }}</NuxtLink>
     <div class="article">
       <NuxtLink to="https://www.vuemastery.com/blog/building-a-3d-scene-in-nuxt-with-tresjs/"> 3D+nuxt结合使用文章链接 </NuxtLink>
       <NuxtLink to="https://tresjs.org/"> 3d官网 </NuxtLink>
     </div>
   </div>
-  <div class="section shadow-sm bg-gray-300">
+  <div class="section shadow-xl bg-zinc-300">
     <NuxtLink to="/three">去three页面</NuxtLink>
   </div>
-  <div class="section shadow-sm bg-gray-300">
+  <div class="section shadow-xl bg-zinc-300">
     <NuxtLink to="/picDemo">去pic页面</NuxtLink>
   </div>
 
   <ClientOnly>
-    <div class="section click_div shadow-sm" @click="toggleDark()"> 当前模式：{{ isDark ? 'dark' : 'light' }} </div>
+    <div class="section click_div shadow-xl bg-zinc-300" @click="toggleDark()"> 当前模式：{{ isDark ? 'dark' : 'light' }} </div>
   </ClientOnly>
 
   <div v-for="item in yy" :key="item.id">
-    <ListItem :title="item.name" :id="item.id" />
+    <ListItem :title="item.name" :id="item.id" class="bg-zinc-300 shadow-xl" />
   </div>
 </template>
 
@@ -56,8 +56,8 @@ const toggleDark = useToggle(isDark);
 .section {
   width: 300px;
   padding: 16px;
-  borader-radius: 4px;
-  margin-bottom: 8px;
+  border-radius: 4px;
+  margin-bottom: 16px;
 }
 .click_div {
   cursor: pointer;
